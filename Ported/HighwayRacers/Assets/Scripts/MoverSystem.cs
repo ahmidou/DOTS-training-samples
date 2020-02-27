fefacac240e41237bf3197a23b98d056bf93c25f
ahmidou <ahmidou@eidosmontreal.com>
2020-02-27 16:12:46 -0500

    enabled burst on the mover

--- Ported/HighwayRacers/Assets/Scripts/MoverSystem.cs
+++ Ported/HighwayRacers/Assets/Scripts/MoverSystem.cs
@@ -18,7 +18,7 @@ public class MoverSystem : JobComponentSystem
     //
     // The job is also tagged with the BurstCompile attribute, which means
     // that the Burst compiler will optimize it for the best performance.
-    //[BurstCompile]
+    [BurstCompile]
     struct MoverSystemJob : IJobForEach<Mover>
     {
         // Add fields here that your job needs to do its work.
