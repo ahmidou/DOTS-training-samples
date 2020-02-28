bdc337f2aa03274b491094a0ae550d4e1430c3d3
Etienne Chenard <etienne.chenard@outlook.com>
2020-02-28 11:59:24 -0500

    busting the speed limiter

--- Ported/HighwayRacers/Assets/Scripts/SpeedLimiterSystem.cs
+++ Ported/HighwayRacers/Assets/Scripts/SpeedLimiterSystem.cs
@@ -11,57 +11,70 @@ using UnityEngine;
 
 public class SpeedLimiter : JobComponentSystem
 {
-    EntityQuery m_Query;
-
-    protected override void OnCreate()
+    [BurstCompile]
+    struct SpeedLimiterJob : IJobForEach<Mover>
     {
-        m_Query = GetEntityQuery(typeof(Mover));
+        // Add fields here that your job needs to do its work.
+        // For example,
+        public Track track;
+
+        public void Execute(ref Mover mover)
+        {
+            // Implement the work to perform for each entity here.
+            // You should only access data that is local or that is a
+            // field on this job. Note that the 'rotation' parameter is
+            // marked as [ReadOnly], which means it cannot be modified,
+            // but allows this job to run in parallel with other jobs
+            // that want to read Rotation component data.
+            // For example,
+            //  translation.Value += mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+
+            if (mover.drivingBehavior == Mover.DrivingBehavior.LimitSpeed)
+            {
+                //mine.speed = math.lerp(mine.frontCarSpeed, mine.speed, mine.frontCarDistance / track.minDistanceToSlowDown);
+                //mine.speed = math.max(mine.frontCarSpeed, mine.speed *(mine.frontCarDistance / track.minDistanceToSlowDown + 1));
+                mover.speed = mover.frontCarSpeed;
+            }
+            else if (mover.drivingBehavior == Mover.DrivingBehavior.Overtake)
+            {
+                mover.speed = mover.baseSpeed * 1.2f;
+            }
+            else if (mover.speed != mover.baseSpeed)
+            {
+                if (mover.speed > mover.baseSpeed)
+                {
+                    mover.speed -= 0.05f;
+                    if (mover.speed < mover.baseSpeed)
+                    {
+                        mover.speed = mover.baseSpeed;
+                    }
+                }
+                else
+                {
+                    mover.speed += 0.05f;
+                    if (mover.speed > mover.baseSpeed)
+                    {
+                        mover.speed = mover.baseSpeed;
+                    }
+                }
+            }
+
+            // Debug.Log(mover.distanceOnLane);            
+        }
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
+
+        var job = new SpeedLimiterJob();
         EntityQuery m_Group = GetEntityQuery(typeof(Track));
         var track = m_Group.GetSingleton<Track>();
-        var movers = m_Query.ToComponentDataArray<Mover>(Allocator.TempJob
-            );
+        // Assign values to the fields on your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        job.track = track;
 
-        var jobHandle = Entities
-            .ForEach((Entity entity, ref Mover mine) =>
-            {
-                if (mine.drivingBehavior == Mover.DrivingBehavior.LimitSpeed)
-                {
-                    //mine.speed = math.lerp(mine.frontCarSpeed, mine.speed, mine.frontCarDistance / track.minDistanceToSlowDown);
-                    //mine.speed = math.max(mine.frontCarSpeed, mine.speed *(mine.frontCarDistance / track.minDistanceToSlowDown + 1));
-                    mine.speed = mine.frontCarSpeed;
-                }
-                else if (mine.drivingBehavior == Mover.DrivingBehavior.Overtake)
-                {
-                    mine.speed = mine.baseSpeed * 1.2f;
-                }
-                else if (mine.speed != mine.baseSpeed)
-                {
-                    if (mine.speed > mine.baseSpeed)
-                    {
-                        mine.speed -= 0.05f;
-                        if (mine.speed < mine.baseSpeed)
-                        {
-                            mine.speed = mine.baseSpeed;
-                        }
-                    }
-                    else
-                    {
-                        mine.speed += 0.05f;
-                        if (mine.speed > mine.baseSpeed)
-                        {
-                            mine.speed = mine.baseSpeed;
-                        }
-                    }
-                }
-            })
-            .Schedule(inputDependencies);
-        jobHandle.Complete();
-        movers.Dispose();
-
-        return jobHandle;
+        // Now that the job is set up, schedule it to be run. 
+        return job.Schedule(this, inputDependencies);
     }
 }
