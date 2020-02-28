62294f74bd9f7caa1373f6ba7cbb42ede5c502e6
Mike polydore <dmpolydore@eidosmontreal.com>
2020-02-28 12:04:56 -0500

    fix car spawning

--- Ported/HighwayRacers/Assets/Scripts/HighWay.cs
+++ Ported/HighwayRacers/Assets/Scripts/HighWay.cs
@@ -17,6 +17,12 @@ public class HighWay : MonoBehaviour
     public float minDistanceToSlowDown = 3.0f;
     public float laneDistanceMultiplier = 1.03f;
 
+    public class Spot
+    {
+        public int lane;
+        public float position;
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,19 +42,42 @@ public class HighWay : MonoBehaviour
                 laneDistanceMultiplier = laneDistanceMultiplier
             });
 
+        var spots = new List<Spot>();
+        for (int iLane = 0; iLane < laneCount; iLane++)
+        {
+            for(float x=0;x<length;x+= minDistanceToSlowDown)
+            {
+                spots.Add(new Spot { lane = iLane, position = x});
+            }
+        }
+        if(carCount> spots.Count)
+        {
+            Debug.Log("Overcrowded !!!");
+            return;
+        }
+        
         for (var x = 0; x < carCount; x++)
         {
 
             // Efficiently instantiate a bunch of entities from the already converted entity prefab
-            var instance = entityManager.Instantiate(prefab);   
+            var instance = entityManager.Instantiate(prefab);
 
             // Place the instantiated entity in a grid with some noise
-            var position = new Vector3((float)UnityEngine.Random.Range(0, length), 0 ,0);
+            int currentLane = UnityEngine.Random.Range(0, laneCount);
+
+            Vector3 position;
+
+            int spotIndex = UnityEngine.Random.Range(0, spots.Count);
+
+            currentLane = spots[spotIndex].lane;
+            position = new Vector3(spots[spotIndex].position, 0, 0);
+
+            spots.Remove(spots[spotIndex]);
+
             //Debug.Log(position);
             var speed = UnityEngine.Random.Range(minSpeed, maxSpeed); 
             entityManager.AddComponent<Mover>(instance);
             entityManager.AddComponent<MaterialColor>(instance);
-            int currentLane = UnityEngine.Random.Range(0, laneCount);
             entityManager.SetComponentData(instance, new Mover { speed = speed, distanceOnLane=position.x, currentLane = currentLane, futureLane = currentLane, baseSpeed=speed, laneChangeSpeed=1, laneChangeRatio=0, behaviorStayOnLane=x%2==1 });
             entityManager.SetComponentData(instance, new Translation { Value = position });
         }
